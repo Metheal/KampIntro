57a99146f671ebb64a2371bb0621509cae987d7b
Metheal <metheal@outlook.com>
2021-01-22 18:57:21 +0300

    MyDictionary isimli class eklendi

---
+++ GenericsIntro/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyDictionary<TK, TV>
+    {
+        TK[] keys;
+        TV[] values;
+        public MyDictionary()
+        {
+            keys = new TK[0];
+            values = new TV[0];
+        }
+
+        public void Add(TK key, TV value)
+        {
+            TK[] tempKeys = keys;
+            TV[] tempValues = values;
+            keys = new TK[keys.Length + 1];
+            values = new TV[values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+            //Console.WriteLine("{0} {1} eklendi!", key, value); // test amacli
+        }
+    }
+}
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -10,6 +10,11 @@ namespace GenericsIntro
             isimler.Add("Engin");
 
             Console.WriteLine("Hello World!");
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ilk deger");
+            myDictionary.Add(2, "Ikinci deger");
+            myDictionary.Add(3, "Ucuncu deger");
         }
     }
 }
