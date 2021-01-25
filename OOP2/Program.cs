e884dd9b55bc46750c16c4a9f269df3d6bc38798
Metheal <metheal@outlook.com>
2021-01-25 18:56:25 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Engin Demirog
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.ID = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demirog";
+            musteri1.TcNo = "12345678910";
+
+            // Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.ID = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+
+            // gercek musteri and tuzel musteri are different
+            // thus they can't replace each other
+            // SOLID
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+
+        }
+    }
+}
---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        // method injection
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            // Basvuru bilgilerini degerlendirme
+
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            krediManager.Hesapla();
+            loggerService.Log();
+        }
+
+        public void KrediOnbilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+            }
+        }
+    }
+}
